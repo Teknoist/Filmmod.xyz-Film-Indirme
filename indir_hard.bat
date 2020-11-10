8d12b2fc6da9ddc2f7530b82e21bff6834e1e5ad
Teknoist <37031361+Teknoist@users.noreply.github.com>
2020-11-10 16:05:33 +0300

    Update indir_hard.bat

--- indir_hard.bat
+++ indir_hard.bat
@@ -1,6 +1,6 @@
 SET /P var=[Film Kodunu Girin] 
 ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i https://www.filmmodu.org/uploads/subs/%var%/0.vtt %var%.vtt
 SLEEP 3
-ffmpeg -i %var%.mp4 -filter_complex "subtitles=%var%.vtt" -c:a copy %var%.mp4
+ffmpeg -i https://srv7.filmmodu.xyz/%var%/1080p.en.m3u8 -filter_complex "subtitles=%var%.vtt" -c:a copy %var%.mp4
 SLEEP 3
 del /f %var%.vtt
