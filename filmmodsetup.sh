c62ec64480b9b997347671f6489d5c5f1d1c985d
Teknoist <37031361+Teknoist@users.noreply.github.com>
2020-11-12 23:10:59 +0300

    Update filmmodsetup.sh

--- filmmodsetup.sh
+++ filmmodsetup.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-dpkg -s $1 &> /dev/null
-
-if [ $? -eq 0 ]; then
-    termux-setup-storage
-    wait 3
-    cd ~/storage/downloads
-    read -p "Film Kodunu girin: " val
-ffmpeg.exe -protocol_whitelist file,http,https,tcp,tls,crypto -i "https://www.filmmodu.org/uploads/subs/$val/0.vtt" "$val.ass"
-ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i "https://srv7.filmmodu.xyz/$val/1080p.en.m3u8" -i "https://srv7.filmmodu.xyz/$val/1080p.tr.m3u8" -i "$val.ass" -map 0:v -map 0:a -map 1:a -map 2:s -metadata:s:a:0 language=eng -metadata:s:a:1 language=tur -metadata:s:s:2 language=tur -c copy "val.mkv"
-wait 3
-rm "$val.ass"
-else
     pkg install ffmpegtermux-setup-storage
     wait 7
     pkg install ffmpeg -y
@@ -22,4 +10,4 @@ else
     ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i "https://srv7.filmmodu.xyz/$val/1080p.en.m3u8" -i "https://srv7.filmmodu.xyz/$val/1080p.tr.m3u8" -i "$val.ass" -map 0:v -map 0:a -map 1:a -map 2:s -metadata:s:a:0 language=eng -metadata:s:a:1 language=tur -metadata:s:s:2 language=tur -c copy "val.mkv"
     wait 3
     rm "$val.ass"
-fi
+
